f3028a5d3428391bc346d835ab41f50436696d4a
SATHU <e15330@eng.pdn.ac.lk>
2021-02-07 01:25:27 +0530

    build added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const userRouter = require('./routes/userRoutes');
 const lessonRouter = require('./routes/lessonRoutes');
 const mongoose = require('mongoose');
+const path = require('path');
 var cors = require('cors')
 
 
@@ -19,5 +20,9 @@ app.use(cors())
 app.use('/user', userRouter);
 app.use('/lessons',lessonRouter);
 const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'client\\lms-client-app\\build')));
 
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client\\lms-client-app\\build', 'index.html'));
+});
 app.listen(port,() => console.log('server started'))
